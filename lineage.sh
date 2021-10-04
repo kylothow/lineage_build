61a08a79f431377a725e9dfa319e0df1c3bfc580
Michele Beccalossi <beccalossi.michele@gmail.com>
2021-10-04 08:56:46 +0200

    Sign builds with private keys
    Signed-off-by: Michele Beccalossi <beccalossi.michele@gmail.com>

--- lineage.sh
+++ lineage.sh
@@ -18,4 +18,11 @@ cd ../..
 source build/envsetup.sh
 mka clean -j$(nproc)
 lunch lineage_dumpling-user
-mka bacon -j$(nproc)
+mka target-files-package otatools -j$(nproc)
+sign_target_files_apks -o -d ~/.android-certs \
+    $OUT/obj/PACKAGING/target_files_intermediates/*-target_files-*.zip \
+    signed-target_files.zip
+ota_from_target_files -k ~/.android-certs/releasekey \
+    --block --backup=true \
+    signed-target_files.zip \
+    signed-ota_update.zip
